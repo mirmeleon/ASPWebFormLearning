938a2eb8d8e96205c6a5f7c3a571b373f1253983
mirmeleon <morgensterndenise@gmail.com>
2017-12-03 15:21:53 +0100

    create pdf with web forms

---
+++ CreatingPdf/CreatingPdf/01.pdf.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="01.pdf.aspx.cs" Inherits="CreatingPdf._01_pdf" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Id" DataSourceID="SqlDataSource1">
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="Id" InsertVisible="False" ReadOnly="True" SortExpression="Id" />
+                    <asp:BoundField DataField="Title" HeaderText="Title" SortExpression="Title" />
+                    <asp:BoundField DataField="StartDate" HeaderText="StartDate" SortExpression="StartDate" />
+                    <asp:CheckBoxField DataField="IsActive" HeaderText="IsActive" SortExpression="IsActive" />
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Chats]"></asp:SqlDataSource>
+            <asp:Button ID="Button1" runat="server" Text="Export pdf" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CreatingPdf/CreatingPdf/01.pdf.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using System.IO;
+
+namespace CreatingPdf
+{
+    public partial class _01_pdf : System.Web.UI.Page
+    {
+        //sourceforge.net/projects/itextsharp ->extraktvame foldera i ot neia ni triabva samo 
+      //  dll-core; sega triabva da se dobavi referencia kum tazi dll-ka vuv nashia proekt ot vs
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                string cs = ConfigurationManager.ConnectionStrings[""].ConnectionString;
+
+
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter("Select * from tblEmployee", con);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    GridView1.DataSource = ds;
+                    GridView1.DataBind();
+
+                }
+                
+            }
+           
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //suzdavame pdf table, kato ia pravim s tolkova cols kolkoto cols ima grid viewto ni, ama zimame headera shtoto purvoachalno cols sa prazni
+            
+            PdfPTable pdfTable = new PdfPTable(GridView1.HeaderRow.Cells.Count);
+            //zimame headera
+            
+                foreach (TableCell headerCell in GridView1.HeaderRow.Cells)
+                {
+                Font font = new Font();
+                //zima cveta na gridViewto
+                font.Color = new BaseColor(GridView1.HeaderStyle.ForeColor);
+
+
+                    PdfPCell pdfCell = new PdfPCell(new Phrase(headerCell.Text, font));
+                pdfCell.BackgroundColor = new BaseColor(GridView1.HeaderStyle.BackColor);
+                pdfTable.AddCell(pdfCell);
+                }
+            
+
+            //zimame redovete ot gridviewto ni
+            foreach (GridViewRow row in GridView1.Rows)
+            {
+                foreach (TableCell tableCell in row.Cells)
+                {
+                    Font font = new Font();
+                    //zima cveta na gridViewto
+                    font.Color = new BaseColor(GridView1.RowStyle.ForeColor);
+                                        //creatvame pdf cell sega i ia populatvame
+                    PdfPCell pdfCell = new PdfPCell(new Phrase(tableCell.Text));
+                    pdfCell.BackgroundColor = new BaseColor(GridView1.RowStyle.BackColor);
+                    pdfTable.AddCell(pdfCell);
+                }
+            }
+
+            Document pdfDocument = new Document(PageSize.A4, 10f,10f,10f,10f);
+            //outputstreama ni pozvoliava da go downloadnem, a ako iskame da go zapishem v servera ni (v papka v sluchaia) 
+            //polzvame filestream
+            // PdfWriter.GetInstance(pdfDocument, Response.OutputStream);
+
+            //zapazvane na servera, mappath ni pozvoliava da si opravim putia
+            PdfWriter.GetInstance(pdfDocument, new FileStream(Server.MapPath("~/PdfDocs/Employees.pdf"), FileMode.Create));
+
+            pdfDocument.Open();
+            pdfDocument.Add(pdfTable);
+            pdfDocument.Close();
+            //specificirame content type-a v headera za da moje da se otvori pdfa
+            //dolnite linii triabvat samo ako otvariame pdf-a, varianta s output stream
+            //Response.ContentType = "application/pdf";
+            //Response.AppendHeader("content-disposition", "attachment; filename=Employees.pdf");
+            //Response.Write(pdfDocument);
+            //Response.Flush();
+            //Response.End();
+
+
+        }
+    }
+}
